39c4e4ac4b51662bf7b52228cee4f1caa2e352bf
carcisar <carmenciscararroyo@gmail.com>
2024-07-31 11:29:37 +0200

    Add crud product

---
+++ products-services/src/main/java/com/mcca/products_services/ProductsServicesApplication.java
@@ -0,0 +1,13 @@
+package com.mcca.products_services;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductsServicesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductsServicesApplication.class, args);
+	}
+
+}
---
+++ products-services/src/test/java/com/mcca/products_services/ProductsServicesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mcca.products_services;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductsServicesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
